f07681e828f0d2c3f5ba8725a0ec1448fd10903e
imagine10255 <imagine10255@gmail.com>
2022-02-16 10:03:44 +0800

    feat: 新增 themeName

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export {default as Row} from './components/Row';
 export {default as Flex} from './components/Row/Flex';
 export {default as Col} from './components/Col';
 export {default as GridThemeProvider} from './components/GridThemeProvider';
+export * from './config';
 
 export * from './media/types';
 export * from './components/Col/types';
