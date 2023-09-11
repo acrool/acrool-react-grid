424b366141cd19cd68221838d5161d90f0e600ff
imagine10255 <imagine10255@gmail.com>
2023-09-11 23:39:03 +0800

    fix: fix styled-component vite 設定導致 @media 讀取不到 version

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
         },
         cssTarget: 'chrome61',
         rollupOptions: {
-            external: ['react', 'react-dom'],
+            external: ['react', 'react-dom', 'styled-components'],
             output: {
                 globals: {
                     react: 'React',
