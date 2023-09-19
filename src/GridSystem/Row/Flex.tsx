0465be6d2807b86c5d921277b62f96d887c566cc
imagine10255 <imagine10255@gmail.com>
2023-09-19 08:45:42 +0800

    fix remove Flex

--- src/GridSystem/Row/Flex.tsx
+++ src/GridSystem/Row/Flex.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import CSS from 'csstype';
-import {TRowVertical, TRowHorizontal, TFlexDirection} from '../../types';
+import {TFlexDirection} from '../../types';
 
 
 /**
@@ -10,14 +10,10 @@ const Flex = styled.div<{
     style?: CSS.Properties,
     className?: string,
     direction?: TFlexDirection
-    vertical?: TRowVertical; // 垂直
-    horizontal?: TRowHorizontal; // 水平
 }>`
   box-sizing: border-box;
   display: flex;
   flex-direction: ${props => props.direction ? props.direction : 'row'};
-  justify-content: ${props => props.horizontal ? props.horizontal : 'left'};
-  align-items: ${props => props.vertical ? props.vertical : 'top'};
 `;
 
 export default Flex;
